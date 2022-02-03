76b65eb85b3a47093b33de8c60cd863d7302e8b1
Don Yu <donyu@Don-iMac.home>
2022-02-03 04:30:28 -0600

    moved directory and shop data into reducer, created selectors respectively

--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -1,56 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors'
 
 import MenuItem from '../menu-item/menu-item'
 
 import './directory.scss'
 
-class Directory extends React.Component {
-  constructor() {
-    super()
+const Directory = ({ sections }) => (
+  <div className='directory-menu'>
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+)
 
-    this.state = {
-      sections: [{
-        title: 'hats',
-        imageUrl: 'https://i.ibb.co/zPWRRX9/hats.jpg',
-        id: 1,
-        linkUrl: 'hats'
-      },
-      {
-        title: 'jackets',
-        imageUrl: 'https://i.ibb.co/5h37dJD/jackets.jpg',
-        id: 2,
-        linkUrl: ''
-      },
-      {
-        title: 'sneakers',
-        imageUrl: 'https://i.ibb.co/Lpb874Q/sneaker.jpg',
-        id: 3,
-        linkUrl: ''
-      },
-      {
-        title: 'womens',
-        imageUrl: 'https://i.ibb.co/dpw4Cth/women.jpg',
-        size: 'large',
-        id: 4,
-        linkUrl: ''
-      },
-      {
-        title: 'mens',
-        imageUrl: 'https://i.ibb.co/M5yNT2r/men.jpg',
-        size: 'large',
-        id: 5,
-        linkUrl: ''
-      }]
-    }
-  }
 
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (<MenuItem key={id} {...otherSectionProps} />))}
-      </div>
-    )
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+})
 
-export default Directory
+export default connect(mapStateToProps)(Directory)
